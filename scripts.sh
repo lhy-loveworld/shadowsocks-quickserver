e8d541d6f08f740d1ba5c053a302e96bbd74ac0f
lhy-loveworld <hylipeace@gmail.com>
2021-01-20 04:46:04 +0000

    Fixes unexpected new line.

--- scripts.sh
+++ scripts.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-curl https://github.com/shadowsocks/shadowsocks-rust/releases/download/v
-1.8.23/shadowsocks-v1.8.23.x86_64-unknown-linux-gnu.tar.xz -LO
+curl https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.8.23/shadowsocks-v1.8.23.x86_64-unknown-linux-gnu.tar.xz -LO
 
 tar -xvf shadowsocks-v1.8.23.x86_64-unknown-linux-gnu.tar.xz
 
