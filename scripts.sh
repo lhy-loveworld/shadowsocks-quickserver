bceec31eb83713e748f0244f7b0b7ddb5ea7d074
lhy-loveworld <hylipeace@gmail.com>
2021-01-20 04:41:44 +0000

    Adds the commands and config example.

---
+++ scripts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl https://github.com/shadowsocks/shadowsocks-rust/releases/download/v
+1.8.23/shadowsocks-v1.8.23.x86_64-unknown-linux-gnu.tar.xz -LO
+
+tar -xvf shadowsocks-v1.8.23.x86_64-unknown-linux-gnu.tar.xz
+
+./ssserver -c config.json
